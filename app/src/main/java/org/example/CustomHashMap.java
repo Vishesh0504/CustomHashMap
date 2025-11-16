d329d44c54326a402d82441867b70e4bd26157d2
vishesh <visheshdpanchal@gmail.com>
2025-11-16 23:44:51 +0530

    feat:iterator implemented

--- app/src/main/java/org/example/CustomHashMap.java
+++ app/src/main/java/org/example/CustomHashMap.java
@@ -1,13 +1,30 @@
 package org.example;
 
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
-class Node<K, V> {
+
+class Node<K, V> implements Map.Entry<K, V> {
     int hash;
     K key;
     V value;
     Node<K, V> next;
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V newValue) {
+        V oldValue = this.value;
+        this.value = newValue;
+        return oldValue;
+    }
 
     Node(int hash, K key, V value) {
         this.hash = hash;
@@ -16,9 +33,10 @@ class Node<K, V> {
     }
 }
 
-public class CustomHashMap<K, V> implements Map<K, V> {
-    private int size;
 
+public class CustomHashMap<K, V> {
+    private int size;
+    private int modCount;
     private final double loadFactor = 0.75;
     private Node<K, V>[] table;
     private int threshold;
@@ -26,6 +44,104 @@ public class CustomHashMap<K, V> implements Map<K, V> {
     CustomHashMap() {
         size = 0;
         table = null;
+        modCount = 0;
+    }
+
+    private abstract class HashIterator<E> implements Iterator<E> {
+        int index;
+        Node<K, V>[] tableRef;
+        Node<K, V> next;
+        Node<K, V> prev;
+        Node<K, V> lastReturned;
+        int lastReturnedIndex;
+        int expectedModCount;
+
+        private int scanForNext(int index) {
+            for (int i = index; i < tableRef.length; i++) {
+                if (tableRef[i] != null) {
+                    return i;
+                }
+            }
+            return tableRef.length;
+        }
+
+        public HashIterator() {
+            this.expectedModCount = modCount;
+            tableRef = table == null ? (Node<K, V>[]) new Node[0] : table;
+            index = scanForNext(0);
+            lastReturned = null;
+            lastReturnedIndex = -1;
+            prev = null;
+            next = index == tableRef.length ? null : tableRef[index];
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+            return next != null;
+        }
+
+        protected abstract E element(Node<K, V> node);
+
+        @Override
+
+        public E next() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (next == null) {
+                throw new NoSuchElementException();
+            }
+            Node<K, V> returned = next;
+            int returnedBucket = index;
+
+            Node<K, V> computedNext;
+            int computedBucketIndex;
+            if (returned.next == null) {
+                computedBucketIndex = scanForNext(returnedBucket + 1);
+                computedNext = computedBucketIndex < tableRef.length ? tableRef[computedBucketIndex] : null;
+            } else {
+                computedNext = returned.next;
+                computedBucketIndex = returnedBucket;
+            }
+
+            if (returned.next == null) {
+                prev = null;
+                next = computedNext;
+                index = computedBucketIndex;
+            } else {
+                prev = returned;
+                next = computedNext;
+
+            }
+            lastReturned = returned;
+            lastReturnedIndex = returnedBucket;
+            return element(returned);
+        }
+
+        @Override
+        public void remove() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (lastReturned == null) {
+                throw new IllegalStateException();
+            }
+            Node<K, V> target = lastReturned;
+            int bucket = lastReturnedIndex;
+            if (prev == null) {
+                table[bucket] = target.next;
+            } else {
+                prev.next = target.next;
+            }
+            lastReturned = null;
+            lastReturnedIndex = -1;
+            modCount++;
+            expectedModCount++;
+            size--;
+        }
     }
 
     public int size() {
@@ -56,6 +172,7 @@ public class CustomHashMap<K, V> implements Map<K, V> {
 
         if (table[bucketIdx] == null) {
             table[bucketIdx] = new Node<K, V>(hash, key, value);
+            modCount++;
             size++;
             return null;
         } else {
@@ -72,10 +189,12 @@ public class CustomHashMap<K, V> implements Map<K, V> {
             }
             if (prev != null) {
                 prev.next = new Node<K, V>(hash, key, value);
+                modCount++;
                 size++;
             }
         }
         if (size >= threshold) {
+            modCount++;
             resize();
         }
         return null;
@@ -155,4 +274,72 @@ public class CustomHashMap<K, V> implements Map<K, V> {
         }
         return null;
     }
+
+    //    @Override
+    public V remove(Object key) {
+        if (table == null || table.length == 0) return null;
+
+        int h = hash(key);
+        int bucketIdx = h & (table.length - 1);
+
+        Node<K, V> node = table[bucketIdx];
+        Node<K, V> prev = null;
+
+        while (node != null) {
+            if (node.hash == h && Objects.equals(key, node.key)) {
+                if (prev == null) {
+                    table[bucketIdx] = node.next;
+                } else {
+                    prev.next = node.next;
+                }
+                modCount++;
+                size--;
+                return node.value;
+            }
+            prev = node;
+            node = node.next;
+        }
+        return null;
+    }
+
+    //    @Override
+    public boolean containsKey(Object key) {
+        return get(key) != null || (get(key) == null && containsKeyWithNullValue(key));
+    }
+
+    private boolean containsKeyWithNullValue(Object key) {
+        if (table == null || table.length == 0) return false;
+        int h = hash(key);
+        int bucketIdx = h & (table.length - 1);
+        Node<K, V> node = table[bucketIdx];
+        while (node != null) {
+            if (node.hash == h && Objects.equals(key, node.key)) {
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    private class KeyIterator extends HashIterator<K> {
+        @Override
+        protected K element(Node<K, V> node) {
+            return node.key;
+        }
+    }
+
+    private class ValueIterator extends HashIterator<V> {
+        @Override
+        protected V element(Node<K, V> node) {
+            return node.value;
+        }
+    }
+
+    private class EntryIterator extends HashIterator<Map.Entry<K, V>> {
+        @Override
+        protected Map.Entry<K, V> element(Node<K, V> node) {
+            return node;
+        }
+    }
+
 }
